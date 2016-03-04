6e865b444838bdb25e806e65983b218a6c991591
Yauheni Liohenki <legenkiyes@gmail.com>
2016-03-04 12:41:39 +0300

    Init Commit
    Init Commit

---
+++ AndroidGitHubCoach/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidGitHubCoach
+{
+    [Activity(Label = "AndroidGitHubCoach", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
